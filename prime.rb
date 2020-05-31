de9f525e31cf891018556e4daebabf8ac96a9569
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-31 20:30:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,17 @@
 def prime?(num)
 
-if num <= 1
-  false
-else 
-  
-counter = 2
-  while counter < num 
-   if num % counter == 0
-    return false
-   end
-    counter += 1
-   end
-true
-end
+  if num <= 1
+    false
+  else 
+    
+  counter = 2
+    while counter < num 
+     if num % counter == 0
+      return false
+     end
+      counter += 1
+     end
+  true
+  end
 end
   
